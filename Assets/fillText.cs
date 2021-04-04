798010ccc1188b87afde10460fd214315efe091b
lsatoshi <leandro.satoshi@usp.br>
2021-04-04 16:22:03 -0300

    comitao geladaço

---
+++ Assets/fillText.cs
@@ -0,0 +1,23 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using TMPro;
+
+public class fillText : MonoBehaviour {
+    public string text;
+    public TextMeshProUGUI dialogueText;
+    public int framesPerCharacter;
+
+    public void Start() {
+        StartCoroutine(FillText());
+    }
+
+    private IEnumerator FillText() {
+        foreach (char character in text) {
+            dialogueText.text = dialogueText.text + character;
+
+            for (int i = 0; i < framesPerCharacter; i++)
+                yield return null;
+        }
+    }
+}
